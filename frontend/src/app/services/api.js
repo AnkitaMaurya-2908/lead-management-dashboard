f55510622f82e6a30aef91e27204dda9504e7512
Amit Maurya <ankitamaurya2908@gmail.com>
2026-01-16 23:39:30 +0530

    chore: configure API base url

--- frontend/src/app/services/api.js
+++ frontend/src/app/services/api.js
@@ -1,7 +1,16 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: "http://localhost:5001",
+// });
+
+// export default API;
+
+
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5001",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
 export default API;
